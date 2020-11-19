d58a5c07dfa20aed01c6abb32413437ba69d79e1
Olakunle Jegede <jkunle@gmail.com>
2020-11-19 12:10:19 +0000

    Revert "Hub-737: tear down shorthub abtest and keep variant"

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,20 +1,11 @@
 #!/usr/bin/env bash
 
-set -e
+set -eu
 
 if [[ "${1:-}" == "--no-parallel" ]]
 then
     SHOW_BROWSER=${SHOW_BROWSER:-true}
     INSTANCES=1
-    shift
-fi
-
-if [[ ! -z $1 ]]
-then
-  bundle --quiet
-  mkdir -p testreport
-  SHOW_BROWSER=${SHOW_BROWSER:-false} TEST_ENV=${TEST_ENV:-local} bundle exec cucumber $* --strict --tags 'not @ignore'
-  exit $?
 fi
 
 bundle --quiet
